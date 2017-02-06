f74316e91c2d48ad78e4a6ba8f366cae99186791
Lawrence <Lawrence4code@gmail.com>
2017-02-06 12:25:03 +0530

    Add POST/todos route and refactor mongoose with changes

---
+++ server/tests/server.test.js
@@ -0,0 +1,34 @@
+const expect = require('expect');
+const request = require('supertest');
+
+var {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+beforeEach((done) => {
+    Todo.remove({}).then(() => done());
+});
+
+describe('POST /todos', () => {
+    it('should create a new todo', (done) => {
+        var text = 'Test Todo Text';
+        
+        request(app)
+            .post('/todos')
+            .send({text})
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.text).toBe(text);
+            })
+            .end((error, res) => {
+                if(error) {
+                    return console.log('Error at getting the response back.'), done(error);
+                }
+
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(1);
+                    expect(todos[0].text).toBe(text);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+});
